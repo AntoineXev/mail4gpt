c294ea446957617c7487a4fe876cce71007de900
Antoine H <52413145+AntoineXev@users.noreply.github.com>
2024-05-20 20:16:01 +0000

    Added entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
