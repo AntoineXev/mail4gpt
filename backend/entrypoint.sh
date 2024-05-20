43e4bf57877bbcf5589a4212b812b2f95fa36b5c
Antoine H <52413145+AntoineXev@users.noreply.github.com>
2024-05-20 21:06:06 +0000

    Added user

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,7 +7,8 @@ python manage.py collectstatic --noinput
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
-
+echo "Your auth token"
+python manage.py adduser
 # Start server
 echo "Starting server"
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000
